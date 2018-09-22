c2a575bd0d82ec2572c7bb35211739269ea3b26d
hayamimashiro <hayamimashiro@gmail.com>
2018-09-22 22:51:03 +0800

    modify

---
+++ 002Console/js/alert.js
@@ -0,0 +1,14 @@
+/*function woDeLaoLu() {
+    alert("0=======haha");
+    alert("1=======huhu");
+    alert("2=======mmama");
+    alert("3=======onesann");
+    alert("4=======shio");
+    alert("5=======papa");
+}
+
+woDeLaoLu();
+woDeLaoLu();*/
+
+alert("hfalhfaohfao");
+
